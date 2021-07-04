b8b8052cf3645cbbed38a9cb0fa75f9c34238d5d
KleinerKobold <trollabeissa@gmail.com>
2021-07-04 15:58:09 +0200

    added port and directory

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-docker run --rm epikur:latest \
+docker run \
+       --rm \
+       -d \
+       -v /home/oliver/docker/epikurDocker/EpikurServer:/root/EpikurServer \
+       -p 9980:8080 \
+       --name epikurServer \
+       epikur:latest \
        /usr/share/epikur4Server/bin/start.sh
